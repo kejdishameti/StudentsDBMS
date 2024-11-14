5a3495fa8aabda23289d3e071196cdfe7a078315
Kejdi <kejdishameti@gmail.com>
2024-11-14 14:23:40 +0100

    Added Students model for student data

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentsDBMS.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Grade { get; set; } = string.Empty;
+    }
+}
