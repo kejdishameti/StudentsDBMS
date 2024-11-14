b68fcd5219250f21b6695be06e0a325d4304fb38
Kejdi <kejdishameti@gmail.com>
2024-11-14 14:56:01 +0100

    Added Display Menu for the main program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            static void DisplayMenu()
+            {
+                while (true)
+                {
+                    Console.WriteLine("\nStudents DBMS");
+                    Console.WriteLine("1. Show Students Table");
+                    Console.WriteLine("2. Add record");
+                    Console.WriteLine("3. Update record");
+                    Console.WriteLine("4. Delete record");
+                    Console.WriteLine("5. Exit");
+                    Console.Write("Press one number to perform an action: ");
+                    string? input = Console.ReadLine();
+
+                    if(string.IsNullOrEmpty(input))
+                    {
+                        Console.WriteLine("Invalid input, please try again.");
+                        continue;
+                    }
+
+                    switch(input)
+                    {
+                        case "1":
+                            ShowStudentsTable();
+                            break;
+                        case "2":
+                            AddRecord();
+                            break;
+                        case "3":
+                            EditRecord();
+                            break;
+                        case "4":
+                            DeleteRecord();
+                            break;
+                        case "5":
+                            Console.WriteLine("Exiting the application...");
+                            return;
+                        default:
+                            Console.WriteLine("Invalid option, please enter a valid number");
+                            break;
+                    }    
+                }
+            }
         }
     }
 }
