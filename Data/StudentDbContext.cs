72054cdc009abb167f62b670195d603e3d001510
Kejdi <kejdishameti@gmail.com>
2024-11-14 14:35:16 +0100

    Added StudentDbContext to configure SQlite database

---
+++ Data/StudentDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentsDBMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsDBMS.Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=C:\Users\Kejdi\Desktop\sqliteDbDemo\mydatabase.db");
+        }
+    }
+}
