8a08b9167c1e60a63729338698d9d9133859dd9e
Kejdi <kejdishameti@gmail.com>
2024-11-14 13:49:13 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentsDBMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
